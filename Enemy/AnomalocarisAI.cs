d9cb84fa7c1b52e1168f9337bb31a93a77dffdf2
Ccode-lang <coop112097@gmail.com>
2024-11-18 11:22:54 -0700

    Add project files.

---
+++ Enemy/AnomalocarisAI.cs
@@ -0,0 +1,154 @@
+﻿using GameNetcodeStuff;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace Anomalocaris.Enemy
+{
+    internal class AnomalocarisAI : EnemyAI
+    {
+        // No states needed since they are just thousand year old water shrimp
+
+        private QuicksandTrigger water;
+        private BoxCollider waterCollider;
+        private readonly QuicksandTrigger[] sandAndWater = FindObjectsOfType<QuicksandTrigger>();
+        private readonly List<QuicksandTrigger> waters = new List<QuicksandTrigger>();
+
+        private float wanderTimer;
+        private Vector3 wanderPos = Vector3.zero;
+
+        private bool wallInFront;
+
+        [SerializeField] private Transform RaycastPos;
+        private const float Speed = 5;
+
+        public override void Start()
+        {
+            base.Start();
+            if (!IsServer) return;
+
+            try
+            {
+                foreach (QuicksandTrigger maybeWater in sandAndWater)
+                {
+                    if (maybeWater.isWater && !maybeWater.gameObject.CompareTag("SpawnDenialPoint"))
+                    {
+                        waters.Add(maybeWater);
+                    }
+                }
+
+                if (waters.Count == 0)
+                {
+                    RoundManager.Instance.DespawnEnemyOnServer(new NetworkObjectReference(gameObject.GetComponent<NetworkObject>()));
+                    return;
+                }
+
+                if (TimeOfDay.Instance.currentLevelWeather == LevelWeatherType.Flooded)
+                {
+                    RoundManager.Instance.DespawnEnemyOnServer(new NetworkObjectReference(gameObject.GetComponent<NetworkObject>()));
+                    return;
+                }
+
+                water = waters[UnityEngine.Random.Range(0, waters.Count)];
+                waterCollider = water.gameObject.GetComponent<BoxCollider>();
+
+                SetWanderPos();
+
+                transform.position = new Vector3(water.transform.position.x, waterCollider.bounds.max.y, water.transform.position.z);
+            } catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
+
+        private void SetWanderPos()
+        {
+            wanderPos.x = UnityEngine.Random.Range(waterCollider.bounds.min.x, waterCollider.bounds.max.x);
+            wanderPos.y = UnityEngine.Random.Range(waterCollider.bounds.min.y, waterCollider.bounds.max.y);
+            wanderPos.z = UnityEngine.Random.Range(waterCollider.bounds.min.z, waterCollider.bounds.max.z);
+
+            wanderTimer = 0f;
+        }
+
+        private static bool Collision(Vector3 pos, Collider col)
+        {
+            return col.bounds.Contains(pos);
+        }
+
+        private void TurnTowardsLocation3d(Vector3 location)
+        {
+            transform.LookAt(location);
+        }
+
+        private bool CheckForWall()
+        {
+            return Physics.Raycast(RaycastPos.position, RaycastPos.forward, 4f, 1 << 8 /**Bitmasks are weird. This references layer 8 which is "Room"**/);
+        }
+
+        private void Rise(float riseSpeed)
+        {
+            transform.Translate(Vector3.up * (riseSpeed * Time.deltaTime));
+        }
+
+        private static float WaterTop(BoxCollider coll)
+        {
+            return coll.transform.localScale.y * coll.size.y / 2 + coll.transform.position.y;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (!IsServer) return;
+
+            if (isEnemyDead && transform.position.y < waterCollider.bounds.max.y)
+                Rise(0.2f);
+
+            if (isEnemyDead)
+                return;
+
+            wanderTimer += Time.deltaTime;
+
+            base.DoAIInterval();
+            if (WaterTop(waterCollider) < transform.position.y)
+                transform.position = new Vector3(transform.position.x, WaterTop(waterCollider), transform.position.z);
+
+
+            if (wanderTimer >= 5)
+            {
+                SetWanderPos();
+            }
+
+            Vector3 wanderLocation = Vector3.MoveTowards(transform.position, wanderPos, Speed * Time.deltaTime);
+
+            TurnTowardsLocation3d(wanderPos);
+
+            if (wallInFront || !Collision(wanderLocation, waterCollider))
+            {
+                SetWanderPos();
+            }
+            else
+            {
+                transform.position = wanderLocation;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (!IsServer) return;
+            wallInFront = CheckForWall();
+        }
+
+        public override void HitEnemy(int force = 1, PlayerControllerB playerWhoHit = null, bool playHitSFX = false, int hitID = -1)
+        {
+            base.HitEnemy(force, playerWhoHit, playHitSFX, hitID);
+            enemyHP -= force;
+            if (enemyHP <= 0)
+            {
+                KillEnemyOnOwnerClient();
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,68 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using LethalLib;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using static LethalLib.Modules.Enemies;
+
+namespace Anomalocaris
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    [BepInDependency(LethalLib.Plugin.ModGUID)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "com.ccode.anomalocaris";
+        public const string NAME = "Anomalocaris";
+        public const string VERSION = "0.0.1";
+
+        public static ManualLogSource Log;
+
+        public static EnemyType AnomalocarisEnemy;
+
+        public void Awake()
+        {
+            Assets.PopulateAssets();
+
+            AnomalocarisEnemy = Assets.MainAssetBundle.LoadAsset<EnemyType>("AnomalocarisEnemy");
+            var Node = Assets.MainAssetBundle.LoadAsset<TerminalNode>("AnomalocarisTN");
+            var Keyword = Assets.MainAssetBundle.LoadAsset<TerminalKeyword>("AnomalocarisKW");
+
+            RegisterEnemy(AnomalocarisEnemy, 999, LethalLib.Modules.Levels.LevelTypes.All, SpawnType.Daytime, Node, Keyword);
+            Log = Logger;
+            Log.LogInfo(NAME + " mod version " + VERSION + " loaded.");
+
+            // netcode patcher
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                var methods = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+                foreach (var method in methods)
+                {
+                    var attributes = method.GetCustomAttributes(typeof(RuntimeInitializeOnLoadMethodAttribute), false);
+                    if (attributes.Length > 0)
+                    {
+                        method.Invoke(null, null);
+                    }
+                }
+            }
+        }
+    }
+
+    public static class Assets
+    {
+        public static AssetBundle MainAssetBundle = null;
+        public static void PopulateAssets()
+        {
+            string sAssemblyLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            MainAssetBundle = AssetBundle.LoadFromFile(Path.Combine(sAssemblyLocation, "anomalocarisassets"));
+            if (MainAssetBundle == null)
+            {
+                Plugin.Log.LogError("Failed to load custom assets.");
+                return;
+            }
+        }
+    }
+}
